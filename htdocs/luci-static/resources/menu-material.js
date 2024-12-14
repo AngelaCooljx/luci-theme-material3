e89bfb5ab4d76223db22cc3c3fcc21805a06f1c8
cool <bilibili@att.net>
2024-12-15 02:09:21 +0800

    Format code using VSCode

--- htdocs/luci-static/resources/menu-material.js
+++ htdocs/luci-static/resources/menu-material.js
@@ -3,13 +3,13 @@
 'require ui';
 
 return baseclass.extend({
-	__init__: function() {
+	__init__: function () {
 		ui.menu.load().then(L.bind(this.render, this));
 	},
 
-	render: function(tree) {
+	render: function (tree) {
 		var node = tree,
-		    url = '';
+			url = '';
 
 		this.renderModeMenu(node);
 
@@ -28,7 +28,7 @@ return baseclass.extend({
 
 		document.querySelector('.darkMask')
 			.addEventListener('click', ui.createHandlerFn(this, 'handleSidebarToggle'));
-			
+
 		document.querySelector(".main > .loading").style.opacity = '0';
 		document.querySelector(".main > .loading").style.visibility = 'hidden';
 
@@ -36,13 +36,13 @@ return baseclass.extend({
 			document.querySelector('.main-left').style.width = '0';
 
 		window.addEventListener('resize', this.handleSidebarToggle, true);
-		
+
 	},
 
-	handleMenuExpand: function(ev) {
+	handleMenuExpand: function (ev) {
 		var a = ev.target, ul1 = a.parentNode, ul2 = a.nextElementSibling;
 
-		document.querySelectorAll('li.slide.active').forEach(function(li) {
+		document.querySelectorAll('li.slide.active').forEach(function (li) {
 			if (li !== a.parentNode || li == ul1) {
 				li.classList.remove('active');
 				li.childNodes[0].classList.remove('active');
@@ -66,26 +66,26 @@ return baseclass.extend({
 		ev.stopPropagation();
 	},
 
-	renderMainMenu: function(tree, url, level) {
+	renderMainMenu: function (tree, url, level) {
 		var l = (level || 0) + 1,
-		    ul = E('ul', { 'class': level ? 'slide-menu' : 'nav' }),
-		    children = ui.menu.getChildren(tree);
+			ul = E('ul', { 'class': level ? 'slide-menu' : 'nav' }),
+			children = ui.menu.getChildren(tree);
 
 		if (children.length == 0 || l > 2)
 			return E([]);
 
 		for (var i = 0; i < children.length; i++) {
 			var isActive = (L.env.dispatchpath[l] == children[i].name),
-			    submenu = this.renderMainMenu(children[i], url + '/' + children[i].name, l),
-			    hasChildren = submenu.children.length;
+				submenu = this.renderMainMenu(children[i], url + '/' + children[i].name, l),
+				hasChildren = submenu.children.length;
 
-			ul.appendChild(E('li', { 'class': (hasChildren ? 'slide' + (isActive ? ' active' : '') : (isActive ? ' active' : ''))}, [
+			ul.appendChild(E('li', { 'class': (hasChildren ? 'slide' + (isActive ? ' active' : '') : (isActive ? ' active' : '')) }, [
 				E('a', {
 					'href': hasChildren ? '#' : L.url(url, children[i].name),
 					'class': hasChildren ? 'menu' + (isActive ? ' active' : '') : null,
 					'click': hasChildren ? ui.createHandlerFn(this, 'handleMenuExpand') : null,
 					'data-title': hasChildren ? null : _(children[i].title),
-				}, [ _(children[i].title) ]),
+				}, [_(children[i].title)]),
 				submenu
 			]));
 		}
@@ -100,9 +100,9 @@ return baseclass.extend({
 		return ul;
 	},
 
-	renderModeMenu: function(tree) {
+	renderModeMenu: function (tree) {
 		var ul = document.querySelector('#modemenu'),
-		    children = ui.menu.getChildren(tree);
+			children = ui.menu.getChildren(tree);
 
 		for (var i = 0; i < children.length; i++) {
 			var isActive = (L.env.requestpath.length ? children[i].name == L.env.requestpath[0] : i == 0);
@@ -111,26 +111,26 @@ return baseclass.extend({
 				E('a', {
 					'href': L.url(children[i].name),
 					'class': isActive ? 'active' : null
-				}, [ _(children[i].title) ])
+				}, [_(children[i].title)])
 			]));
 
 			if (isActive)
 				this.renderMainMenu(children[i], children[i].name);
 
 			if (i > 0 && i < children.length)
-				ul.appendChild(E('li', {'class': 'divider'}, [E('span')]))
+				ul.appendChild(E('li', { 'class': 'divider' }, [E('span')]))
 		}
 
 		if (children.length > 1)
 			ul.parentElement.style.display = '';
 	},
 
-	renderTabMenu: function(tree, url, level) {
+	renderTabMenu: function (tree, url, level) {
 		var container = document.querySelector('#tabmenu'),
-			    l = (level || 0) + 1,
-			    ul = E('ul', { 'class': 'tabs' }),
-			    children = ui.menu.getChildren(tree),
-			    activeNode = null;
+			l = (level || 0) + 1,
+			ul = E('ul', { 'class': 'tabs' }),
+			children = ui.menu.getChildren(tree),
+			activeNode = null;
 
 		if (children.length == 0)
 			return E([]);
@@ -141,7 +141,7 @@ return baseclass.extend({
 				className = 'tabmenu-item-%s %s'.format(children[i].name, activeClass);
 
 			ul.appendChild(E('li', { 'class': className }, [
-				E('a', { 'href': L.url(url, children[i].name) }, [ _(children[i].title) ] )
+				E('a', { 'href': L.url(url, children[i].name) }, [_(children[i].title)])
 			]));
 
 			if (isActive)
@@ -157,18 +157,18 @@ return baseclass.extend({
 		return ul;
 	},
 
-	handleSidebarToggle: function(ev) {
+	handleSidebarToggle: function (ev) {
 		var width = window.innerWidth,
-		    darkMask = document.querySelector('.darkMask'),
-		    mainRight = document.querySelector('.main-right'),
-		    mainLeft = document.querySelector('.main-left'),
-		    open = mainLeft.style.width == '';
+			darkMask = document.querySelector('.darkMask'),
+			mainRight = document.querySelector('.main-right'),
+			mainLeft = document.querySelector('.main-left'),
+			open = mainLeft.style.width == '';
 
 		if (width > 1152 || ev.type == 'resize')
 			open = true;
-		
+
 		darkMask.style.visibility = open ? '' : 'visible';
-		darkMask.style.opacity = open ? '': 1;
+		darkMask.style.opacity = open ? '' : 1;
 
 		if (width <= 1152)
 			mainLeft.style.width = open ? '0' : '';
